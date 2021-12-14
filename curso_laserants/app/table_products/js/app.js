d53a1ad764ec2b7a6108792c1436c59f0ac9008f
José Sánchez <jose.venados6@gmail.com>
2021-12-13 18:10:14 -0600

    Pure functions

--- curso_laserants/app/table_products/js/app.js
+++ curso_laserants/app/table_products/js/app.js
@@ -1,28 +1,29 @@
 const preloadedState = { producto: {}, productos: [] };
 
-const store = Redux.createStore(producto_store.reducer, preloadedState);
+const middlewares = Redux.applyMiddleware(
+    logger_middleware,
+    agregar_o_modificar_producto_middleware,
+    generador_codigo_producto_builder(0),
+    );
 
-let latest_state;
+const store = Redux.createStore(reducer, preloadedState, middlewares);
 
-store.subscribe( () => {
-    let current_state = store.getState();
-    if (latest_state != current_state) {
-        latest_state = current_state;
-        ui.render_form(current_state.producto);
-        ui.render_table(current_state.productos);
+store.subscribe(dispatch_on_change(store.getState, (state) => {
+    ui.render_form(state.producto);
+    ui.render_table(state.productos);
+}));
+
+ui.onFormSubmit = (producto) => store.dispatch(agregar_o_modificar_producto(producto));
+ui.on_eliminar_click = (index) => store.dispatch(producto_eliminado(index));
+ui.on_editar_click = (index) => store.dispatch(producto_seleccionado(index));
+
+function dispatch_on_change(getState, dispatch){
+    let latest_state;
+    return function(){
+        let current_state = getState();
+        if (latest_state != current_state) {
+            latest_state = current_state;
+            dispatch(current_state);
+        }
     }
-});
-
-ui.onFormSubmit = (producto) => {
-    if (producto.index) {
-        store.dispatch(producto_store.producto_modificado(producto));
-    }else{
-        store.dispatch(producto_store.producto_agregado(producto));
-    }
-
-    store.dispatch(producto_store.producto_seleccionado(null));
-};
-
-ui.on_eliminar_click = (index) => store.dispatch(producto_store.producto_eliminado(index));
-
-ui.on_editar_click = (index) => store.dispatch(producto_store.producto_seleccionado(index));
+}
--- curso_laserants/app/table_products/js/store.js
+++ curso_laserants/app/table_products/js/store.js
@@ -1,67 +1,41 @@
-// Others
-let index = 0;
-
-const reducer = (state, action) => {
-    if (action.type == 'producto-agregado') {
-        index++;
-        let producto = action.payload;
-        let total = producto.cantidad * producto.precio;
-        return {
-            ...state,
-            productos: [...state.productos, {
-                ...producto,
-                index,
-                total
-            }]
-        };
-    }
-
-    if (action.type == 'producto-modificado') {
-        const producto = action.payload;
-        const productos = state.productos.slice();
-        const codigo = producto.codigo;
-        const total = producto.precio * producto.cantidad;
-        const old = productos.find((item) => item.codigo == codigo);
-        const indice = productos.indexOf(old);
-        productos[indice] = { ...producto, total };
-        return {
-            ...state,
-            productos
-        }
-    }
-
-    if (action.type == 'producto-eliminado') {
-        const codigo = action.payload.index;
-        const productos = state.productos.filter((item) => item.index != codigo);
-        return {
-            ...state,
-            productos
-        }
-    }
-
-    if (action.type == 'producto-seleccionado') {
-        const codigo = action.payload.index;
-        return {
-            ...state,
-            producto: state.productos.find(x => x.index == codigo) || {}
-        }
-    }
-
-    return state;
+const ActionTypes = {
+    productoAgregado : 'producto-agregado',
+    productoModificado : 'producto-modificado',
+    productoEliminado : 'producto-eliminado',
+    productoSeleccionado : 'producto-seleccionado',
+    productoAgregadoOModificado : 'producto-agregado-o-modificado'
 };
 
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case ActionTypes.productoAgregado:
+            return producto_agregado_reducer(state, action);
+        case ActionTypes.productoModificado:
+            return producto_modificado_reducer(state, action);
+        case ActionTypes.productoEliminado:
+            return producto_eliminado_reducer(state, action);
+        case ActionTypes.productoSeleccionado:
+            return producto_seleccionado_reducer(state, action);
+        default:
+            return state;
+    }    
+};
+
+/* FUNCTIONS */
+
 const producto_seleccionado = (index) => ({
-    type: 'producto-seleccionado',
+    type: ActionTypes.productoSeleccionado,
     payload: { index }
 });
 
 const producto_eliminado = (index) => ({
-    type: 'producto-eliminado',
+    type: ActionTypes.productoEliminado,
     payload: { index }
 });
 
 const producto_modificado = (data) => ({
-    type: 'producto-modificado',
+    type: ActionTypes.productoModificado,
     payload: {
         index: data.index,
         nombre: data.nombre,
@@ -72,7 +46,7 @@ const producto_modificado = (data) => ({
 });
 
 const producto_agregado = (data) => ({
-    type: 'producto-agregado',
+    type: ActionTypes.productoAgregado,
     payload: {
         nombre: data.nombre,
         cantidad: data.cantidad,
@@ -81,10 +55,98 @@ const producto_agregado = (data) => ({
     }
 });
 
-const producto_store = {
-    reducer,
-    producto_seleccionado,
-    producto_eliminado,
-    producto_modificado,
-    producto_agregado,
+const agregar_o_modificar_producto = (data) => ({
+    type: ActionTypes.productoAgregadoOModificado,
+    payload: {
+        nombre: data.nombre,
+        cantidad: data.cantidad,
+        precio: data.precio,
+        categoria: data.categoria
+    }
+});
+
+
+/* REDUCER FUNCTIONS */
+
+function producto_agregado_reducer(state, action) {
+    let producto = action.payload;
+    let total = producto.cantidad * producto.precio;
+    return {
+        ...state,
+        productos: [...state.productos, {
+            ...producto,
+            total
+        }]
+    };
 }
+
+function producto_modificado_reducer(state, action) {
+    const producto = action.payload;
+    const productos = state.productos.slice();
+    const codigo = producto.codigo;
+    const total = producto.precio * producto.cantidad;
+    const old = productos.find((item) => item.codigo == codigo);
+    const indice = productos.indexOf(old);
+    productos[indice] = { ...producto, total };
+    return {
+        ...state,
+        productos
+    };
+}
+
+function producto_seleccionado_reducer(state, action) {
+    const codigo = action.payload.index;
+    return {
+        ...state,
+        producto: state.productos.find(x => x.index == codigo) || {}
+    };
+}
+
+function producto_eliminado_reducer(state, action) {
+    const codigo = action.payload.index;
+    const productos = state.productos.filter((item) => item.index != codigo);
+    return {
+        ...state,
+        productos
+    };
+}
+
+/* MIDDLEWARES */
+function generador_codigo_producto_builder(last_id){
+    let index = last_id;
+    return store => next => action => {
+        if (action.type != ActionTypes.productoAgregado) {
+            return next(action);
+        }
+
+        index++;  
+
+        const action_to_dispatch = {
+            ...action,
+            payload: {
+                ...action.payload,
+                index
+            }
+        };
+
+        return next(action_to_dispatch);
+    };
+}
+
+const logger_middleware = store => next => action => {
+    console.log('dispatching', action);
+    const result = next(action);
+    return result;
+};
+
+const agregar_o_modificar_producto_middleware = store => next => action => {
+    if (action.type != ActionTypes.productoAgregadoOModificado) {
+        return next(action);
+    }
+
+    const producto = action.payload;
+    let action_to_dispatch = producto.index ? producto_modificado(producto) : producto_agregado(producto);
+
+    store.dispatch(action_to_dispatch);
+    return store.dispatch(producto_seleccionado(null));
+};
